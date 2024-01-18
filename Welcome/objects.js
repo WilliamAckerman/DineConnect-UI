2232380d1924f110166e4a41dbf887bb5e02f792
WilliamAckerman <148882361+WilliamAckerman@users.noreply.github.com>
2024-01-17 19:22:02 -0500

    DineConnect

---
+++ Welcome/objects.js
@@ -0,0 +1,52 @@
+const user1 = {
+    userID:"001",
+    username:"User1",
+    email:"User1@blah.com",
+    password:"user1Pass",
+    phone:"111-222-3333"
+};
+const user2 = {
+    userID:"002",
+    username:"User2",
+    email:"User2@blah.com",
+    password:"user2Pass",
+    phone:"444-555-6666"
+};
+const user3 = {
+    userID:"003",
+    username:"User3",
+    email:"User3@blah.com",
+    password:"user2Pass",
+    phone:"777-888-9999"
+};
+
+const rest1 = {
+    restaurantID:"101",
+    name:"Good Bites",
+    address:"341 Green Road",
+    cuisine:"Greek",
+    avgrating:"4.1"
+};
+
+const item1 = {
+    itemID:"201",
+    name:"Rice",
+    description:"Basic rice",
+    price:"3.99"
+};
+
+const order1 = {
+    orderID:"301",
+    user:"User2",
+    restaurant:"Good Bites",
+    items:"Rice",
+    totalPrice:"3.99"
+};
+
+const feedback1 = {
+    feedbackID:"401",
+    user:"User3",
+    restaurant:"Good Bites",
+    rating:4,
+    comment:"I like it."
+};
